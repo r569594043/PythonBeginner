1df81d87138732c809005120e4cf46f223e8f544
RileyRen <rxb123b@qq.com>
2014-01-07 18:06:39 +0800

    modify class sample

--- 06.Class.py
+++ 06.Class.py
@@ -1,12 +1,62 @@
 #-*- encoding: utf-8 -*-
 
-var = 4
+class Animal():
+	"""Animal Class"""
 
-if var % 2 == 1:
-	print("It's a odd number")
-else:
-	print("It's a even number")
+	name = "Animal"
+
+	def __init__(self):
+		self.className = 'Animal'
+
+class Cat(Animal):
+	"""Cat Class"""
+	name = 'Cat'
+
+	def __init__(self, name):
+		super(Animal, self).__init__()
+		self.className = 'Cat'
+		self.name = name
+	def shout(self):
+		print(self.name + ' shout: miao~')
+	def favorite():
+		print('my favorite food is fish')
 
 
-for i in range(0, 10):
-	print("hello world")
+class Dog(Animal):
+	"""Dog Class"""
+	def __init__(self):
+		super(Animal, self).__init__()
+		self.className = 'Dog'
+	def shout(self):
+		print(self.name + ' shout: wang~')
+	def power():
+		print('my super power is watch your house')
+
+
+
+kitty = Cat('kitty')
+kitty.shout()
+print(kitty.name)
+print(Cat.name)
+print(Animal.name)
+
+wangcai = Dog()
+# dog.name = 'wangcai'
+Dog.name = 'wangcai'
+wangcai.shout()
+print(wangcai.name)
+print(Dog.name)
+
+
+
+
+# Mix
+class Mix(Cat, Dog):
+	def __init__(self, name):
+		super.__init__(Cat, self, name)
+		super.__init__(Dog, self)
+
+mix = Mix('mix')
+mix.shout()
+mix.power()
+mix.favorite()
