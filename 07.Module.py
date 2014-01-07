300016101066beb003de1efc9c12a49981b002e5
RileyRen <rxb123b@qq.com>
2014-01-07 21:50:50 +0800

    complete module sample

--- 07.Module.py
+++ 07.Module.py
@@ -1,12 +1,26 @@
 #-*- encoding: utf-8 -*-
 
-var = 4
+import ModuleA
+from ModuleB import func
+from ModuleB import func1 as b_func1
+import ModuleB
+import ModuleC
+# import ModuleC.Class2
 
-if var % 2 == 1:
-	print("It's a odd number")
-else:
-	print("It's a even number")
+ModuleA.func() # func in ModuleA
+ModuleA.func1() # func1 in ModuleA
+func() # func in ModuleB
+b_func1() # func1 in ModuleB
 
+c = ModuleB.Class2()
+print(c) # <ModuleB.Class2 object at 0x027A5F50>
+c = ModuleC.Class3()
+print(c) # <ModuleC.Class3 object at 0x027A5F10>
+# Error
+# c = ModuleC.Class4.Class4()
 
-for i in range(0, 10):
-	print("hello world")
+from ModuleC import *
+c = Class3() # <ModuleC.Class3 object at 0x028789F0>
+print(c)
+c = Class4.Class4() # <ModuleC.Class4.Class4 object at 0x02878A10>
+print(c)
